a86b7ef3274a5c77e36610f2bca7be984ceead2f
Max <firejama@gmail.com>
2019-07-31 15:01:27 -0400

    added data binding

--- app/src/main/java/com/blkxltng/aboutme/MainActivity.kt
+++ app/src/main/java/com/blkxltng/aboutme/MainActivity.kt
@@ -1,28 +1,39 @@
 package com.blkxltng.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.blkxltng.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Maurice Gaynor")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        done_button.setOnClickListener {
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(it: View) {
-        nickName_text.text = nickName_edit.text
-        nickName_edit.visibility = View.GONE
-        it.visibility = View.GONE
-        nickName_text.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = binding.nickNameEdit.text.toString()
+            invalidateAll()
+            binding.nickNameEdit.visibility = View.GONE
+            it.visibility = View.GONE
+            binding.nickNameText.visibility = View.VISIBLE
+        }
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(it.windowToken, 0)
